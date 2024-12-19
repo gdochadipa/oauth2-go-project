9f65b3f815451d391441199d76974a1a2b56f325
gdochadipa <gede.ocha@igloohome.co>
2024-12-19 15:01:35 +0800

    make simple proto file

--- pkg/database/postgre.go
+++ pkg/database/postgre.go
@@ -4,7 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 
-	"github.com/gdochadipa/oauth2-go-project/pkg/configs",
+	"github.com/gdochadipa/oauth2-go-project/pkg/configs"
 	_ "github.com/lib/pq"
 )
 
@@ -12,7 +12,7 @@ func NewPostgressConnection(cfg configs.DatabaseConfig) (*sql.DB, error) {
 	connStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
 		cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.DBName)
 
-	db, err = sql.Open("postgres", connStr)
+	db, err := sql.Open("postgres", connStr)
 
 	if err != nil {
 		return nil, err
